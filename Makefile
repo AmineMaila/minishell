6d697a66a480d4a89fde63d6bc2cc05666114fc0
Nabil AZOUZ <nazouz@e3r5p5.1337.ma>
2024-01-29 18:23:27 +0100

    cant candle ctrl c

--- Makefile
+++ Makefile
@@ -3,16 +3,16 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: mmaila <mmaila@student.42.fr>              +#+  +:+       +#+         #
+#    By: nazouz <nazouz@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/01/29 13:42:04 by mmaila            #+#    #+#              #
-#    Updated: 2024/01/29 16:28:27 by mmaila           ###   ########.fr        #
+#    Updated: 2024/01/29 16:33:10 by nazouz           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CC= cc
 
-FLAGS= -Wall -Werror -Wextra -lreadline
+FLAGS= -Wall -Werror -Wextra
 
 SRCS= main.c
 
@@ -23,7 +23,7 @@ NAME= minishell
 all : $(NAME)
 
 $(NAME) : $(OBJS)
-	$(CC) $(FLAGS) $(OBJS) -o $(NAME)
+	$(CC) $(FLAGS) $(OBJS) -lreadline -o $(NAME)
 
 %.o : %.c
 	$(CC) $(FLAGS) -c $< -o $@
--- main.c
+++ main.c
@@ -3,26 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmaila <mmaila@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nazouz <nazouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 13:46:25 by mmaila            #+#    #+#             */
-/*   Updated: 2024/01/29 16:27:19 by mmaila           ###   ########.fr       */
+/*   Updated: 2024/01/29 18:23:05 by nazouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+
+void	sigint_handler();
 
 void	prompt()
 {
 	char	*input;
 
 	input = readline("minishell-4.81$ ");
-	printf("%s\n", input);
+	if (input && *input)
+	{
+		add_history(input);
+		if (!strcmp(input, "exit"))
+			exit(EXIT_SUCCESS);
+		free(input);
+	}
+}
+
+void	read_loop()
+{
+	while (1)
+		prompt();
+}
+
+void	sigint_handler()
+{
+	write(1, "\n", 1);
+	read_loop();
+	return ;
+	// prompt();
+	// printf("you pressed CTRL+C\n");
 }
 
 int main(void)
 {
-	prompt();
+	signal(SIGINT, sigint_handler);
+	read_loop();
 }
