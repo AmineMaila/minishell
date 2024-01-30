252f48967f2592fcc998f151352ca5ee2864fa90
Nabil AZOUZ <nazouz@e3r4p3.1337.ma>
2024-01-30 14:15:02 +0100

    working on lexer

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: nazouz <nazouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 20:25:42 by nazouz            #+#    #+#             */
-/*   Updated: 2024/01/30 12:50:49 by nazouz           ###   ########.fr       */
+/*   Updated: 2024/01/30 13:56:56 by nazouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,23 @@
 # include <fcntl.h>
 # include <signal.h>
 
+typedef struct s_lexer
+{
+	char	*str;
+	int		text;
+	int		token;
+}	t_lexer;
+
 typedef struct s_minishell
 {
-	char	*input;
-	char	**cmd_line;
-	char	***tok_line;
-	char	**parser;
-	int		input_red;
-	int		output_red;
+	char				*input;
+	char				**cmd_line;
+	char				***tok_line;
+	char				**parser;
+	int					input_red;
+	int					output_red;
+	struct s_lexer		*lexer;
+	int					lexer_size;
 }	t_minishell;
 
 
@@ -38,6 +47,7 @@ void	tokenize_input(t_minishell *minishell);
 
 //	LIBFT
 char	**ft_split(char const *s, char c);
+char	*ft_strchr(const char *s, int c);
 
 //	HELPERS
 void	ft_print_matrix(char **matrix);
--- parsing/tokenize.c
+++ parsing/tokenize.c
@@ -6,25 +6,74 @@
 /*   By: nazouz <nazouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 11:56:55 by nazouz            #+#    #+#             */
-/*   Updated: 2024/01/30 12:50:13 by nazouz           ###   ########.fr       */
+/*   Updated: 2024/01/30 14:13:52 by nazouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	get_mode(t_minishell *minishell)
+// void	get_mode(t_minishell *minishell)
+// {
+// 	if (minishell->cmd_line[0][0] == '<' && !minishell->cmd_line[0][1])
+// 		minishell->input_red = 1;
+// 	else if (ft_strchr(minishell->cmd_line[0], '<'))
+// }
+
+void	lexer_search(t_minishell *minishell)
 {
-	if (minishell->cmd_line[0][0] == '<')
+	int			i;
+
+	i = 0;
+	while (i < minishell->lexer_size)
+	{
+		if (minishell->lexer[i].str[0] == '\0')
+			return ;
+		if (ft_strchr(minishell->lexer[i].str, '>')
+			|| ft_strchr(minishell->lexer[i].str, '<')
+			|| ft_strchr(minishell->lexer[i].str, '|'))
+			minishell->lexer->token = 1;
+		if (ft_strchr)
+		
+	}
 }
 
-void	tokenize_input(t_minishell *minishell)
+void	create_lexer(t_minishell *minishell)
 {
 	int		i;
 	
 	minishell->cmd_line = ft_split(minishell->input, ' ');
 	i = 0;
 	while (minishell->cmd_line[i])
+		i++;
+	minishell->lexer_size = i;
+	minishell->lexer = malloc(sizeof(t_lexer) * i);
+	i = 0;
+	while (minishell->cmd_line[i])
 	{
-		get_mode(minishell);
+		minishell->lexer[i].str = minishell->cmd_line[i];
+		minishell->lexer[i].text = 0;
+		minishell->lexer[i].token = 0;
+		i++;
 	}
+}
+
+void	input_lexer(t_minishell *minishell)
+{
+	int		i;
+
+	i = 0;
+	while (i < minishell->lexer_size)
+	{
+		if (ft_strchr(minishell->lexer[i].str, '>')
+			|| ft_strchr(minishell->lexer[i].str, '<')
+			|| ft_strchr(minishell->lexer[i].str, '|'))
+			minishell->lexer->token = 1;
+		i++;
+	}
+}
+
+void	tokenize_input(t_minishell *minishell)
+{
+	
+	
 }
